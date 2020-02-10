a623aaa453a4a911d307c2ea8f2e245f885055f2
professor <jayandorian@gmail.com>
2020-02-09 23:23:58 -0500

    ADD: new post + features

--- posts/generate-data-json.sh
+++ posts/generate-data-json.sh
@@ -11,8 +11,10 @@ jq -s . |\
 # format with id as key
 jq 'map( { (.id|tostring): . } ) | add' > _data.json.tmp
 # sed '$ d' | sed "1 d" | jq . | cat > _data.json
-tac _data.json.tmp | sed '3 s/"no"/"yes"/' | tac |\
-perl -pe 's{no}{++$n == 2 ? "yes" : $&}ge' > _data.json
+tac _data.json.tmp | sed -e '0,/oldest_post/ s/"no"/"yes"/' | tac |\
+# tac _data.json.tmp | sed '3 s/"no"/"yes"/' | tac |\
+sed -e '0,/latest_post/ s/"no"/"yes"/' > _data.json
+# perl -pe 's{no}{++$n == 2 ? "yes" : $&}ge' > _data.json
 rm -f _data.json.tmp 2> /dev/null
 head _data.json
 echo "..."
