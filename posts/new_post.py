4ae039db0961bd07f6115e4ecdf56b400a642154
Jay Andorian <jayandorian@gmail.com>
2022-01-27 12:48:25 -0500

    REA: removed extraneous; fixed posts

--- posts/new_post.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-
-title = input("Post title: ") or "Post title"
-subtext = input("Post subtext: ")
-release = input("Post in X number of days: ") or 10
-
-print(title, subtext, release)
